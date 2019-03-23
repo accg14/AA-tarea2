0442f1c590579e61d9c832bada91eeddf98ba224
GonzaNunezCano <gonzalo.nunez.cano@gmail.com>
2019-03-23 16:44:19 -0300

    ID3 finished

--- a/ID3Algorithm.py
+++ a/ID3Algorithm.py
@@ -1,10 +1,15 @@
 import sys
 
-MIN_LEVEL = 0
-MAX_LEVEL = 4
+min_level = 0
+max_level = 4
 
+pipe = '|'
+
+
+#TEMPORAL
 choices = [['A', 0.25, 0.5, 0.75, 1], ['B', 0.25, 0.5, 0.75, 1], ['C', 0.25, 0.5, 0.75, 1], ['D', 0.25, 0.5, 0.75, 1]]
-elements = [[0.1,0.5,0.1,0.1,'Iris-setosa'], [0.1,0.5,0.1,0.1,'Iris-setosa'], [0.1,0.5,0.1,0.1,'Iris-setosa'], [0.1,0.5,0.1,0.1,'Iris-setosa']]
+global_tuples = [[0.1,0.5,0.1,0.1,'Iris-setosa'],[0.1,0.1,0.4,0.1,'Iris-versicolor'],[0.1,0.1,0.4,0.1,'Iris-versicolor'],[0.1,0.8,0.4,0.5,'Iris-versicolor'],[0.4,0.5,0.4,0.1,'Iris-versicolor'],[0.4,0.1,0.1,0.1,'Iris-setosa'],[0.6,0.5,0.1,0.5,'Iris-setosa']]
+#TEMPORAL
 
 
 def create_flowers_set():
@@ -20,29 +25,35 @@ def verify_uniqueness(flowers_set, tuples):
 	for flower in flowers_set:
 		if (flowers_set[flower] == tuples_lenght):
 			return True, flower
-	return False
+	return False, -1
 
 
-def count_tuples(flowers_set, filtered_tuples):
+def count_tuples(flowers_set, tuples):
 	for tuple in tuples:
 		flowers_set[tuple[-1]] += 1
 
-	return max(flowers_set)
+	max = -1
+	selected_flower = -1
+	for flower in flowers_set:
+		if (max < flowers_set[flower]):
+			max = flowers_set[flower]
+			selected_flower = flower
+	return selected_flower
 
 
-def filter(flowers, elements, condition, branch, level):
-	new_elements = []
+def filter(flowers, tuples, condition, branch, level):
+	new_tuples = []
 	if (branch == 1):
-		for element in elements:
-			if(element[level] < condition[level][branch]):
-				new_elements.append(element)
-				flowers[element[-1]] += 1
+		for tuple in tuples:
+			if(tuple[level] < condition[level][branch]):
+				new_tuples.append(tuple)
+				flowers[tuple[-1]] += 1
 	else:
-		for element in elements:
-			if(element[level] < range(condition[level][branch - 1], condition[level][branch])):
-				new_elements.append(element)
-				flowers[element[-1]] += 1
-	return new_elements
+		for tuple in tuples:
+			if(condition[level][branch - 1] <= tuple[level] and tuple[level] < condition[level][branch]):
+				new_tuples.append(tuple)
+				flowers[tuple[-1]] += 1
+	return new_tuples
 
 
 class Node:
@@ -62,6 +73,8 @@ class Node:
 	def get_childs(self):
 		if not (self.is_leaf):
 			return self.childs
+		else:
+			return -1
 
 
 	def set_childs(self, childs):
@@ -69,23 +82,35 @@ class Node:
 
 
 def ID3(level, tuples):
-	if (level < MAX_LEVEL):
-		unique, flower = is_unique_flower = verify_uniqueness(create_flowers_set(), tuples)
-
-		if (unique):
+	if (level < max_level and tuples):
+		is_unique_flower, flower = verify_uniqueness(create_flowers_set(), tuples)
+		if (is_unique_flower):
 			return Node(True, flower)
 		else:
 			node = Node(False, choices[level][0])
 			childs = {}
-			for branch in range(1, 4):
-				filtered_tuples = self.filter(tuples, choices, branch, level)
+			for branch in range(1, 5):
+				filtered_tuples = filter(create_flowers_set(), tuples, choices, branch, level)
 				childs[choices[level][branch]] = ID3(level + 1, filtered_tuples)
 			node.set_childs(childs)
 			return node
 	else:
-		return Node(True, count_tuples(create_flowers_set(), tuples))
+		if (tuples):
+			return Node(True, count_tuples(create_flowers_set(), tuples))
+		else:
+			return Node(True, count_tuples(create_flowers_set(), global_tuples))
+
+
+def print_tree(indentation, tree):
+	printable = indentation + tree.get_label()
+	print(printable)
+	indentation += pipe
+	childs = tree.get_childs()
+	if (childs != -1):
+		for child in childs:
+			print_tree(indentation, childs[child])
 
 
 if __name__== "__main__":
-	tree = ID3(0, elements)
-	
+	tree = ID3(0, global_tuples)
+	print_tree(pipe, tree)
