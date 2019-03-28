2c9dea8eb4266e57826c3a56efae5971ec46abc6
GonzaNunezCano <gonzalo.nunez.cano@gmail.com>
2019-03-27 21:48:28 -0300

    training y test set

--- c/OrderingDataSet.py
+++ c/OrderingDataSet.py
@@ -1,4 +1,3 @@
-import random
 
 def order_data_set():
   f = open('covtype.data', 'r')
@@ -36,39 +35,44 @@ def order_data_set():
   f6.close()
   f7.close()
 
-def trainning_samples(a, b, percent):
+def trainning_samples(percent):
   samples_id = []
-  while len(samples_id) < percent:
-    x = random.randint(a,b)
-    if not x in samples_id:
-      samples_id.append(x)
+  for i in range(1,round(percent)):
+    samples_id.append(i)
 
   return samples_id
 
 def make_training_and_test_set(file, train_f, test_f, train_samples):
+  f = open(file, 'r')
   i  = 1
-  for line in file:
+  for line in f:
     if i in train_samples:
       train_f.write(line)
     else:
       test_f.write(line)
     i += 1
+  f.close()
 
 
 def get_training_and_test_set():
   lengths = [211840, 283301, 35754, 2747, 9493, 17367, 20510]
   train_f = open('training_set.txt','a')
   test_f = open('test_set.txt', 'a')
-  train_samples = []
-  for length in lengths:
-    train_samples += trainning_samples(1, length, length * 0.8)
 
+  train_samples = []
+  train_samples = trainning_samples(lengths[0] * 0.8)
   make_training_and_test_set('soil_1.txt', train_f, test_f, train_samples)
+  train_samples = trainning_samples(lengths[1] * 0.8)
   make_training_and_test_set('soil_2.txt', train_f, test_f, train_samples)
+  train_samples = trainning_samples(lengths[2] * 0.8)
   make_training_and_test_set('soil_3.txt', train_f, test_f, train_samples)
+  train_samples = trainning_samples(lengths[3] * 0.8)
   make_training_and_test_set('soil_4.txt', train_f, test_f, train_samples)
+  train_samples = trainning_samples(lengths[4] * 0.8)
   make_training_and_test_set('soil_5.txt', train_f, test_f, train_samples)
+  train_samples = trainning_samples(lengths[5] * 0.8)
   make_training_and_test_set('soil_6.txt', train_f, test_f, train_samples)
+  train_samples = trainning_samples(lengths[6] * 0.8)
   make_training_and_test_set('soil_7.txt', train_f, test_f, train_samples)
 
   train_f.close()
