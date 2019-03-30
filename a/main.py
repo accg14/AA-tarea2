b26567fbc686cc089df93da6fd12900994565720
joaguirrem <joaquin.aguirre@fing.edu.uy>
2019-03-30 17:30:35 -0300

    Fix A

--- a/main.py
+++ a/main.py
@@ -1,15 +1,15 @@
 import ID3
-import preProcessing
+import PreProcessing
 
 train_set_file = 'train_set.txt'
 test_set_file = 'test_set.txt'
 
 if __name__== "__main__":
-	attributes_order, quantiles = preProcessing.custom_main()
-	train_tuples = preProcessing.data_order(attributes_order, train_set_file)
+	attributes_order, quantiles = PreProcessing.custom_main()
+	train_tuples = PreProcessing.data_order(attributes_order, train_set_file)
 
 	tree = ID3.init(quantiles, train_tuples)
 
-	test_tuples = preProcessing.data_order(attributes_order, test_set_file)
+	test_tuples = PreProcessing.data_order(attributes_order, test_set_file)
 	result = ID3.verify_tree(tree, test_tuples)
 	print(str(result))
